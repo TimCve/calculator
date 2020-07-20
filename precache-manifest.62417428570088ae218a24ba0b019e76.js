502075099ecf2c84c8a120ac0e49a48c06a7dc5e
Timofey Cvetkov <timofey.cvetkov@protonmail.com>
2020-07-20 15:55:23 +0100

    Updates

---
+++ precache-manifest.62417428570088ae218a24ba0b019e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da38e505af918fee121728204f487ed",
+    "url": "/repo-name/index.html"
+  },
+  {
+    "revision": "f65504878f3b2279e551",
+    "url": "/repo-name/static/css/main.06ecd459.chunk.css"
+  },
+  {
+    "revision": "5d44d30fc9408640644c",
+    "url": "/repo-name/static/js/2.7f82a9ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/repo-name/static/js/2.7f82a9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65504878f3b2279e551",
+    "url": "/repo-name/static/js/main.3d44db63.chunk.js"
+  },
+  {
+    "revision": "5cf62609de0a011388a8",
+    "url": "/repo-name/static/js/runtime-main.bc9a6240.js"
+  }
+]);
